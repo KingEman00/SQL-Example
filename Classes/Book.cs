21d0e057113435571c58f0432b656a9fd398926e
KingEman00 <33557433+KingEman00@users.noreply.github.com>
2018-01-08 14:43:28 -0700

    added comments to book.cs classes

--- Classes/Book.cs
+++ Classes/Book.cs
@@ -9,10 +9,10 @@ namespace SQLExample.Classes
         {
         }
 
-        [PrimaryKey, AutoIncrement]
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public string Author { get; set; }
+        [PrimaryKey, AutoIncrement] // Everytime we add something new the primary key will increase by 1
+        public int ID { get; set; } // Set ID as public string
+        public string Name { get; set; } // Set name as public string
+        public string Author { get; set; } // Set author as public string
 
     }
 }
