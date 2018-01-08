f0c8d0108680a50f0ae3bfa66dcbaaa087c2016f
KingEman00 <33557433+KingEman00@users.noreply.github.com>
2018-01-08 14:51:55 -0700

    created the sqlite database connection code

--- Classes/DatabaseHelper.cs
+++ Classes/DatabaseHelper.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace SQLExample.Classes
 {
     public class DatabaseHelper
@@ -6,5 +8,25 @@ namespace SQLExample.Classes
         public DatabaseHelper()
         {
         }
+
+        public static bool Insert<T>(ref T data, string db_path) // Insert data into the db_path (database) path
+        {
+            using (var conn = new SQLite.SQLiteConnection(db_path)) // Use this sqlite connection to insert our data into the db_path
+            {
+                conn.CreateTable<T>(); // Create a table with the data we have obtained
+
+                if (conn.Insert(data) != 0) // Check the data to see if it does not return empty/zero 
+                    return true; // Return true if there is actually data to be used
+            }
+
+
+            return false; // Otherwise, return false
+        }
+
+        public static List<Book> Read(string db_path)
+        {
+            
+        }
+
     }
 }
